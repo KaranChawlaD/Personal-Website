bc17daf1c9e6e793177fec5a666401f77939113e
KaranChawlaD <karan.chawlad@gmail.com>
2023-08-30 16:38:30 -0400

    Work in progress

---
+++ script.js
@@ -0,0 +1,15 @@
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+function scrollAboutMe() {
+    window.scrollTo({top: 900, behavior: 'smooth'});  
+}
+
+function scrollProjects() {
+    window.scrollTo({top: 1600, behavior: 'smooth'});  
+}
+
+function scrollContactMe() {
+    window.scrollTo({top: 2300, behavior: 'smooth'});  
+}
