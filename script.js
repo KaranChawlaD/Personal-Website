2a85f14b2e7444743940b7b2bd1842d6c5fcc082
KaranChawlaD <karan.chawlad@gmail.com>
2023-08-30 16:44:46 -0400

    Formatting

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function scrollToTop() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
+	window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 function scrollAboutMe() {
-    window.scrollTo({top: 900, behavior: 'smooth'});  
+	window.scrollTo({ top: 900, behavior: 'smooth' });
 }
 
 function scrollProjects() {
-    window.scrollTo({top: 1600, behavior: 'smooth'});  
+	window.scrollTo({ top: 1600, behavior: 'smooth' });
 }
 
 function scrollContactMe() {
-    window.scrollTo({top: 2300, behavior: 'smooth'});  
+	window.scrollTo({ top: 2300, behavior: 'smooth' });
 }
